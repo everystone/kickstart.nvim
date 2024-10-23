131552ae8fda321e81bc4ba6e2a5cc693f0862b3
Eirik Kvarstein <ehk@teleplan.no>
2024-10-23 10:09:11 +0200

    removed lsp signature

--- init.lua
+++ init.lua
@@ -689,8 +689,6 @@ require('lazy').setup({
         },
       }
 
-      require('lsp_signature').setup {}
-
       -- Ensure the servers and tools above are installed
       --  To check the current status of installed tools and/or manually install
       --  other tools, you can run
